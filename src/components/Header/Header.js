15ccc4e75d0fc0bc0a1c7429eea85afe6ec24668
rahilchughtai <rahilchughtai1@gmail.com>
2023-01-31 20:18:49 +0100

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="header" style={{ textAlign: 'center' }} >
             <h1>KaomojiGetter (⌒‿⌒)</h1>
             <p> Copy your favorite Kaomojis and send them to your friends!</p>
-            <p> This project is open source. </p>
+            <p> This project is open-source. Made by Ray. </p>
         </div>
     )
 }
